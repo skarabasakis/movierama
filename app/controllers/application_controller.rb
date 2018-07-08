7d58c8a643c782bfed77315356735a00e840f8eb
Stelios Karabasakis <skarab@skroutz.gr>
2018-07-08 13:36:51 +0300

    Add default 404 handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,        keys: [:username, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false }
+      format.any { head :not_found }
+    end
+  end
 end
